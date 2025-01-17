eec07eff1242d2c2105f202264592a2ff7b5ad70
elokfaiqohh <elokfaiqoh885@gmail.com>
2025-01-18 01:26:18 +0700

    update responsive navbar

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -5,9 +5,11 @@ const Navbar = () => {
   const handleClick = () => {
     setShow(!show);
     // console.log(show);
+    console.log("click");
   };
+// buatkan ketika di klik nav nya disembunyikan, ketika di klik lagi muncul pakai let bukan return
 
-  let menuActive = show ? "left-0" : "-left-full";
+  let menuActive = show ? "right-[-100%]" : "right-0";
 
   return (
     <header className="text-gray-400 bg-gray-900 body-font">
@@ -18,13 +20,14 @@ const Navbar = () => {
           </svg>
           <span className="ml-3 text-xl">Tailblocks</span>
         </a>
-        <nav className={`md:static md:flex-row md:w-auto md:h-full md:translate-y-0 right-0 top-1/2 fixed px-8 py-6 flex flex-wrap items-center text-base justify-center`}>
+        <nav className={`px-8 py-6 flex flex-wrap text-base justify-center flex-col sm:right-0 gap-8 items-start bg-gray-800
+          md:bg-transparent md:static md:gap-0 md:flex-row md:w-auto md:h-full md:translate-y-0 right-0 top-1/2 fixed md:items-center transition-all`}>
           <a className="mr-5 hover:text-white">First Link</a>
           <a className="mr-5 hover:text-white">Second Link</a>
           <a className="mr-5 hover:text-white">Third Link</a>
           <a className="mr-5 hover:text-white">Fourth Link</a>
         </nav>
-        <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-5 focus:outline-none md:right-0 hover:bg-gray-700 rounded text-base mt-3 mb-3 md:mt-0" onClick={handleClick}>Button
+        <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-5 focus:outline-none md:right-0 hover:bg-gray-700 rounded text-base mt-[15px] mb-[15px]" onClick={handleClick}>Button
           <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
