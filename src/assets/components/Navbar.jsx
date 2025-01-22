368fd693bb6468306bcf4545550a604a62b5c1b7
elokfaiqohh <elokfaiqoh885@gmail.com>
2025-01-22 13:23:02 +0700

    add icon menu

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <header className="text-gray-400 bg-gray-900 body-font">
       <div className="container mx-auto justify-between flex flex-wrap p-5 flex-row md:flex-row">
-        <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+        <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0 pr-[23%] md:pr-0">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
@@ -27,11 +27,12 @@ const Navbar = () => {
           <a className="mr-5 hover:text-white">Third Link</a>
           <a className="mr-5 hover:text-white">Fourth Link</a>
         </nav>
-        <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-5 focus:outline-none md:right-0 hover:bg-gray-700 rounded text-base mt-[15px] mb-[15px]" onClick={handleClick}>Button
+        <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-5 focus:outline-none md:right-0 hover:bg-gray-700 rounded text-base mt-[15px] mb-[15px]">Button
           <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
         </button>
+        <i className="ri-menu-fill text-3xl md:hidden block items-center mt-auto mb-auto" onClick={handleClick}></i>
       </div>
     </header>
   )
